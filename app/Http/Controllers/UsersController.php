5652cbc03cbc02c2260a635ab9687b637d6d680a
zhq <zhq_553512382@qq.com>
2019-03-05 17:29:46 +0800

    bin

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 class UsersController extends Controller
 {
-    public function create(){
-        return view('users.create');
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except'=>['show','create','store']
+        ]);
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
     }
 
     public function index(){
         return view('users.index');
+    }
+
+    public function create(){
+        return view('users.create');
     }
 
     public function store(Request $request)
@@ -30,6 +41,36 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    /**
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function edit(User $user){
+        try{
+            $this->authorize('update', $user);
+            return view('users.edit',compact('user'));
+        }catch (AuthorizationException $e){
+            return 1;
+        }
+    }
+
+    public function update(User $user,Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+        $this->authorize('update', $user);
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+            $data['password'] = $request->password;
+        }
+        $user->update($data);
+        session()->flash('success','个人资料更新成功!');
+        return redirect()->route('users.show',$user->id);
     }
 
     public function show(User $user){
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,21 @@ Route::get('/',"StaticPagesController@home")->name('home');
 Route::get('/help',"StaticPagesController@help")->name('help');
 Route::get('/about',"StaticPagesController@about")->name('about');
 Route::get('/signup',"UsersController@create")->name('signup');
+
+
+
 Route::resource('users', 'UsersController');
+
+/**
+ * 上面一句等价于下面的
+ */
+//Route::get('/users', 'UsersController@index')->name('users.index');用户列表
+//Route::get('/users/create', 'UsersController@create')->name('users.create');用户注册
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');//用户查看
+//Route::post('/users', 'UsersController@store')->name('users.store');//用户新增
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');//用户编辑页
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');//用户修改
+//Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');//用户删除
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
